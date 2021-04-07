83af1e48428bbe6811d37e79a878bbb4f4dc2f67
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-04-07 10:50:45 -0300

    Add controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      SignupMailer.confirm_email(@user).deliver
+
+      redirect_to @user, notice: 'Successfully registered!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Successfully updated!'
+    else
+      render :update
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :location, :password, :password_confirmation, :bio)
+  end
+end
