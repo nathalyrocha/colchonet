09038c0c4f910e487d02a940521570bfa1e82e5a
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-04-22 21:32:20 -0300

    Add methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ }, uniqueness: true
 
   has_secure_password
+  before_create :generate_token
+
+  def generate_token
+    self.confirmation_token = SecureRandom.urlsafe_base64
+  end
+
+  def confirm!
+    return if confirmed?
+
+    self.confirmed_at = Time.current
+    self.confirmation_token = ''
+
+    save!
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
 end
