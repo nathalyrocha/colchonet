780a7c9fd9f247c0e1ed7917f9b9cdce294c2c13
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-04-22 21:31:46 -0300

    Add confirmation controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
 
+  resource :confirmation, only: [:show]
+
   root to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_211401) do
+ActiveRecord::Schema.define(version: 2021_04_07_143818) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_04_03_211401) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "confirmed_at"
+    t.string "confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
