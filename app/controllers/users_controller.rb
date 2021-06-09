151ee6d5d94a69a9d67b5f3bd49d10b758cb877a
nathalyrocha <nathalyfroliveira@gmail.com>
2021-06-09 19:21:21 -0300

    Add private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_no_authentication, only: [:new, :create]
+  before_action :can_change, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -34,6 +37,16 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def can_change
+    unless user_signed_in && current_user == user
+      redirect_to user_path(params[:id])
+    end
+  end
+
+  def user
+    @user ||= User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:full_name, :email, :location, :password, :password_confirmation, :bio)
