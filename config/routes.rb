9a7cb9a2d8d0738ffd57bd27fd1dcdfebb9a9bce
nathalyrocha <nathalyfroliveira@gmail.com>
2021-06-14 19:27:30 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :rooms
   resources :users
 
+  resources :rooms do 
+    resources :reviews, only: [:create, :update], module: :rooms
+  end
+  
   resource :confirmation, only: [:show]
   resource :user_sessions, only: [:create, :new, :destroy]
 
